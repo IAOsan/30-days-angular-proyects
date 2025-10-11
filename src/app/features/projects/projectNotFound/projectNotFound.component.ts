3b9d57d8263459f65c8ed824e02aae2d03de01ff
Yaotzin Sanabria <yaosanabria@gmail.com>
2025-10-11 03:20:41 -0600

    feat(projects): add dynamic day routing and component loader

---
+++ src/app/features/projects/projectNotFound/projectNotFound.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-not-found',
+  templateUrl: './projectNotFound.component.html',
+})
+export class ProjectNotFoundComponent {}
