d1c645708506dbeb9dbe481b1ae40fa3284d1448
Yaotzin Sanabria <yaosanabria@gmail.com>
2025-10-12 03:32:21 -0600

    initial project setup and base structure, layout and styles

---
+++ src/app/features/projects/components/day3-todoList/todoList.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todoList.component.html',
+  styleUrl: './todoList.component.css',
+})
+export class TodoListComponent {}
--- src/app/features/projects/project.service.ts
+++ src/app/features/projects/project.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { IProject } from '../../shared/models/project.model';
 import { ProfileCardComponent } from './components/day1-profileCard/profileCard.component';
 import { CounterComponent } from './components/day2-counter/counter.component';
+import { TodoListComponent } from './components/day3-todoList/todoList.component';
 
 const PROJECT_COMPONENTS_MAP = new Map<number, IProject>([
   [
@@ -20,6 +21,15 @@ const PROJECT_COMPONENTS_MAP = new Map<number, IProject>([
       day: 2,
       image: 'images/counter.png',
       component: CounterComponent,
+    },
+  ],
+  [
+    3,
+    {
+      name: 'Todo List',
+      day: 3,
+      image: 'images/counter.png',
+      component: TodoListComponent,
     },
   ],
 ]);
