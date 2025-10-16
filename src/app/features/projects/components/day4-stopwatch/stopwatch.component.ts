94d02c9517e403b8b9b327cb2df7ab93d3cf1dfc
Yaotzin Sanabria <yaosanabria@gmail.com>
2025-10-16 01:13:19 -0600

    feat(timer): implement reset functionality

--- src/app/features/projects/components/day4-stopwatch/stopwatch.component.ts
+++ src/app/features/projects/components/day4-stopwatch/stopwatch.component.ts
@@ -1,7 +1,51 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-stopwatch',
   templateUrl: './stopwatch.component.html',
+  imports: [NgClass],
 })
-export class StopwatchComponent {}
+export class StopwatchComponent implements OnDestroy {
+  protected time = 0;
+  protected status: 'idle' | 'running' | 'paused' | 'reset' = 'idle';
+  intervalID: number | undefined;
+
+  generateMilliseconds(): number {
+    const ms = this.time % 1000;
+    return Math.trunc(ms / 10);
+  }
+
+  generateSeconds(): number {
+    return Math.trunc((this.time / 1000) % 60);
+  }
+
+  generateMinutes(): number {
+    return Math.trunc(this.time / 1000 / 60);
+  }
+
+  padTimeTwoDigits(time: number): string {
+    const timeStr = time.toString();
+
+    return timeStr.length < 2 ? `0${timeStr}` : timeStr;
+  }
+
+  getFormattedTime(): string {
+    const milliseconds = this.padTimeTwoDigits(this.generateMilliseconds());
+    const seconds = this.padTimeTwoDigits(this.generateSeconds());
+    const minutes = this.padTimeTwoDigits(this.generateMinutes());
+
+    return `${minutes} : ${seconds} : ${milliseconds}`;
+  }
+
+  handleStart() {
+    this.status = 'running';
+    this.intervalID = setInterval(() => {
+      this.time += 50;
+    }, 50);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalID) clearInterval(this.intervalID);
+  }
+}
