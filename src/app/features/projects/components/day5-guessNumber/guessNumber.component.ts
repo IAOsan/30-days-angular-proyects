7c113080196f83d6194a3c5eda6eb044e7aa3b73
Yaotzin Sanabria <yaosanabria@gmail.com>
2025-10-17 19:52:39 -0600

    feat(guess-number): show number range hint and input field

--- src/app/features/projects/components/day5-guessNumber/guessNumber.component.ts
+++ src/app/features/projects/components/day5-guessNumber/guessNumber.component.ts
@@ -1,7 +1,58 @@
-import { Component } from "@angular/core";
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CapitalizePipe } from '../../../../shared/pipes/capitalize.pipe';
+
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 100;
+const MAX_ATTEMPTS = 10;
 
 @Component({
-    selector: 'app-guess-number',
-    templateUrl: './guessNumber.component.html'
+  selector: 'app-guess-number',
+  templateUrl: './guessNumber.component.html',
+  imports: [FormsModule, NgClass, CapitalizePipe],
 })
-export class GuessNumberComponent {}
+export class GuessNumberComponent {
+  protected numberToGuess = this.generateRandomNumber();
+  protected status: 'idle' | 'failed' | 'win' = 'idle';
+  protected message = '';
+  protected guessedNumberStatus = '';
+  protected attempts = MAX_ATTEMPTS;
+
+  protected get minNumber(): number {
+    return MIN_NUMBER;
+  }
+
+  protected get maxNumber(): number {
+    return MAX_NUMBER;
+  }
+
+  private generateRandomNumber(): number {
+    const randomNumber = Math.random() * (MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER;
+    return Math.trunc(randomNumber);
+  }
+
+  protected handleSubmit(form: NgForm) {
+    const { guessedNumber } = form.value;
+
+    if (guessedNumber < MIN_NUMBER) return;
+
+    if (guessedNumber !== this.numberToGuess) {
+      const isLowerGuess = guessedNumber < this.numberToGuess;
+      this.guessedNumberStatus = isLowerGuess
+        ? 'You came up short.'
+        : 'You overshot it';
+      this.message = isLowerGuess
+        ? 'Try something bigger.'
+        : 'Try something smaller.';
+      this.status = 'failed';
+      this.attempts--;
+    }
+  }
+
+  protected handleGuessedNumberChange(): void {
+    this.status = 'idle';
+    this.guessedNumberStatus = '';
+    this.message = '';
+  }
+}
